0352548fc821554936263f6a3dc5bc638de9cf5a
WilliamLiangUofT <bill.c.liang@gmail.com>
2025-11-11 17:56:37 -0500

    add loadAllAccountData to Account DAO

--- src/main/java/data_access/AccountDataAccessObject.java
+++ src/main/java/data_access/AccountDataAccessObject.java
@@ -1,20 +1,138 @@
 package data_access;
 
 import entity.Account;
+import entity.Transaction;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import use_case.account.AccountDataAccessInterface;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AccountDataAccessObject implements AccountDataAccessInterface {
-    // Temporary Storage DAO, can incorporate persistent storage later by storing ALL ACCOUNT class fields in JSON
     private Map<String, Account> accountNumberToAccount = new HashMap<>();
+    private Map<String, Account.AccountType> accountTypeHashMap = new HashMap<>();
+    private Map<String, Transaction.TransactionType> transactionTypeHashMap = new HashMap<>();
+    private Map<String, Transaction.TransactionCategory> transactionCategoryHashMap = new HashMap<>();
+    private String filename;
+
+    public AccountDataAccessObject(String filename) {
+        this.filename = filename;
+
+        for (Account.AccountType type : Account.AccountType.values()) {
+            accountTypeHashMap.put(type.toString(), type);
+        }
+
+        for (Transaction.TransactionType type : Transaction.TransactionType.values()) {
+            transactionTypeHashMap.put(type.toString(), type);
+        }
+
+        for (Transaction.TransactionCategory type : Transaction.TransactionCategory.values()) {
+            transactionCategoryHashMap.put(type.toString(), type);
+        }
+    }
+
+    // JSON Structure
+    /*
+    {
+        "ACC12345": {
+            "accountType": "CHECKING",
+            "balance": 1250.50,
+            "transactionList": [
+                {
+                    "amount": 200.0,
+                    "transactionType": "INCOME",
+                    "transactionCategory": "SALARY",
+                    "date": "2025-10-01"
+                },
+                {
+                    "amount": 50.0,
+                    "transactionType": "EXPENSE",
+                    "transactionCategory": "DINING",
+                    "date": "2025-10-02"
+                },
+                {
+                    "amount": 100.0,
+                    "transactionType": "EXPENSE",
+                    "transactionCategory": "GROCERIES",
+                    "date": "2025-10-03"
+                }
+            ]
+        },
+        "ACC67890": {
+            "accountType": "SAVINGS",
+            "balance": 5000.00,
+            "transactionList": [
+                {
+                    "amount": 300.0,
+                    "transactionType": "INCOME",
+                    "transactionCategory": "SALARY",
+                    "date": "2025-10-05"
+                },
+                {
+                    "amount": 100.0,
+                    "transactionType": "EXPENSE",
+                    "transactionCategory": "UTILITIES",
+                    "date": "2025-10-08"
+                }
+            ]
+        }
+    }
+     */
+
+    // Read all accounts from JSON file and load them all into HashMap
+    private void loadAllAccountData() {
+        try {
+            String contents = Files.readString(Path.of(this.filename)); // in JSON format
+            JSONObject accountNumbersToAccount = new JSONObject(contents);
+            for (String accountNumberKey : accountNumbersToAccount.keySet()) {
+                JSONObject accountObj = accountNumbersToAccount.getJSONObject(accountNumberKey);
+
+                Account.AccountType accountType = this.accountTypeHashMap.get(accountObj.getString("accountType"));
+                double balance = accountObj.getDouble("balance");
+
+                List<Transaction> transactions = new ArrayList<>();
+                JSONArray transactionList = accountObj.getJSONArray("transactionList");
+                for (int i = 0; i < transactionList.length(); i++) {
+                    JSONObject transactionObj = transactionList.getJSONObject(i);
+                    double transactionAmount = transactionObj.getDouble("amount");
+                    Transaction.TransactionType transactionType = this.transactionTypeHashMap.get(transactionObj.getString("transactionType"));
+                    Transaction.TransactionCategory transactionCategory = this.transactionCategoryHashMap.get(transactionObj.getString("transactionCategory"));
+                    LocalDate transactionDate = LocalDate.parse(transactionObj.getString("date"));
+
+                    Transaction theTransaction = new Transaction(
+                            transactionAmount,
+                            transactionType,
+                            transactionCategory,
+                            transactionDate,
+                            accountNumberKey
+                    );
+                    transactions.add(theTransaction);
+                }
+
+                Account account = new Account(accountNumberKey, accountType, transactions, balance);
+                this.accountNumberToAccount.put(accountNumberKey, account);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to Read Account Data JSON file");
+        }
+    }
+
+    // Save a single newly added account into JSON file
+    private void saveAccountData() {
+
+    }
 
     @Override
     public void saveAccount(Account account) {
         this.accountNumberToAccount.put(account.getAccountNumber(), account);
+        // saveAccountData()
     }
 
     @Override
