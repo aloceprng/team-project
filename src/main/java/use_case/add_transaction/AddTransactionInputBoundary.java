9bb216290c5d044a8975e3a0961b2028dbfc2aca
javifriki <104594403+javifriki@users.noreply.github.com>
2025-11-11 11:50:24 -0500

    Revert "Add transaction use case"

--- src/main/java/use_case/add_transaction/AddTransactionInputBoundary.java
+++
@@ -1,7 +0,0 @@
-package use_case.add_transaction;
-
-public interface AddTransactionInputBoundary {
-
-    void execute (AddTransactionInputData addTransactionInputData);
-
-}
--- src/main/java/use_case/add_transaction/AddTransactionOutputBoundary.java
+++
@@ -1,6 +0,0 @@
-package use_case.add_transaction;
-
-public interface AddTransactionOutputBoundary {
-    void prepareTransactionSuccessView (AddTransactionOutputData addTransactionOutputData);
-    void prepareTransactionFailView (String errorMessage);
-}
